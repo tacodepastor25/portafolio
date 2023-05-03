b6c16d891a817c317327524e25dca634d19ddee2
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2023-05-02 19:28:01 -0600

    Update formumail.php

--- formumail.php
+++ formumail.php
@@ -7,7 +7,7 @@ if(isset($_POST["submit"]))
     $correo = $_POST("email");
     $mensaje = $_POST("mensaje");
     $headers = "MIME-Version: 1.0\r\n";
- 
+    $headers = "Content-type: text/html; charset=utf8\r\n";
     $cuerpo = " Envidado por: $nomb\n Email: $correo\n Mensaje: $mensaje\n";
 
     $bool = mail($para,$asunto,$cuerpo,$headers);
